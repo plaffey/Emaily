d05e79d41cd9f3e69c8912c16c8b37e281269032
Peter Laffey <peter@brainspace.com>
2018-02-12 00:16:52 -0600

    Users can see survey results

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,9 @@ import reduxThunk from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers";
+// Use axios for testing endpoint with OAuth properties
+import axios from "axios";
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
